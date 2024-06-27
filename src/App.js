a464a0c8757c533154a090fca4faff6575f4d45a
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-06-27 18:32:26 +0200

    feat: add new menu tab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+
+// import style for the app
 import "./styles.css";
 
+// menu content array
 const content = [
   [
     "React is extremely popular",
@@ -28,39 +31,55 @@ const content = [
 ];
 
 export default function App() {
+  // active index with get and setter
   const [activeContentIndex, setActiveContentIndex] = useState(0);
 
   return (
     <div>
+      {/* header of the page */}
       <header>
+        {/* app logo */}
         <img src="react-logo-xs.png" alt="React logo" />
         <div>
+          {/* title */}
           <h1>React.js</h1>
           <p>i.e., using the React library for rendering the UI</p>
         </div>
       </header>
 
+      {/* tabs */}
       <div id="tabs">
         <menu>
+          {/* why react button */}
           <button
             className={activeContentIndex === 0 ? "active" : ""}
             onClick={() => setActiveContentIndex(0)}
           >
             Why React?
           </button>
+          {/* core features button */}
           <button
             className={activeContentIndex === 1 ? "active" : ""}
             onClick={() => setActiveContentIndex(1)}
           >
             Core Features
           </button>
+          {/* related resources button */}
           <button
             className={activeContentIndex === 2 ? "active" : ""}
             onClick={() => setActiveContentIndex(2)}
           >
             Related Resources
           </button>
+          {/* react vs js button */}
+          <button
+            className={activeContentIndex === 3 ? "active" : ""}
+            onClick={() => setActiveContentIndex(3)}
+          >
+            React vs JS
+          </button>
         </menu>
+        {/* here i display the content item based on the clicked tab */}
         <div id="tab-content">
           <ul>
             {content[activeContentIndex].map((item) => (
